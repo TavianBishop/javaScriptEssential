bffd8b18e9fb75b39d83343be0e0e303bd3bb144
TavianBishop <Bishopcoding@gmail.com>
2026-07-16 17:35:56 -0400

    product survey

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,33 @@
+const submitButton=document.getElementById('submitBtn');
+
+submitButton.onclick = submitFeedback;
+
+function submitFeedback(){
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+  
+
+
+    document.getElementById('userName').innerText = username;
+    document.getElementById('userAge').innerText = age;
+    document.getElementById('userEmail').innerText = email;
+    document.getElementById('userJob').innerText = job;
+    document.getElementById('userDesignation').innerText = designation;
+    document.getElementById('userProductChoice').innerText = productType;
+    document.getElementById('userFeedback').innerText = feedback;
+    alert('Thank you for feedback');
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      submitFeedback();
+    }
+  });
